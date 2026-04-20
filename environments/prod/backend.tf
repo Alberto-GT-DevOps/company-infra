2b85e167a25dfc8a1d381b51319c46c4b2967220
Alberto <monroyalberto@gmail.com>
2026-04-19 20:58:42 -0600

    Add tags to network module

--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "bto-tf-state-unique-id"
-    key          = "prod/terraform.tfstate" # Path within the bucket
+    key          = "prod/terraform.tfstate"
     region       = "us-west-2"
     use_lockfile = true
     encrypt      = true
