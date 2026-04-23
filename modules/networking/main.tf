224667e4c31eca3ca8004266a4bdb04137109d29
Alberto <monroyalberto@gmail.com>
2026-04-22 20:45:12 -0600

    Add Internet to VPC

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -36,6 +36,8 @@ resource "aws_subnet" "public_subnets" {
 
   tags = merge(local.common_tags, {
     Name = "${local.name_prefix}-public-subnet-${count.index + 1}"
+    "kubernetes.io/role/elb" = "1"
+    "kubernetes.io/cluster/${local.name_prefix}-eks" = "shared"
   })
 }
 
@@ -46,5 +48,69 @@ resource "aws_subnet" "private_subnets" {
   availability_zone = data.aws_availability_zones.available.names[count.index]
   tags = merge(local.common_tags, {
     Name = "${local.name_prefix}-private-subnet-${count.index + 1}"
+    "kubernetes.io/role/internal-elb" = "1"
+    "kubernetes.io/cluster/${local.name_prefix}-eks" = "shared"
   })
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(local.common_tags, {
+    Name = "${local.name_prefix}-igw"
+  })
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = merge(local.common_tags, {
+    Name = "${local.name_prefix}-public-rt"
+  })
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  count = length(var.public_subnet_cidrs)
+  subnet_id = aws_subnet.public_subnets[count.index].id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_eip" "nat" {
+  domain = vpc
+  tags = local.common_tags
+}
+
+resource "aws_nat_gateway" "main_nat_gateway" {
+  allocation_id = aws_eip.nat.id
+  subnet_id = aws_subnet.public_subnets[0].id
+
+  tags = merge(local.common_tags, {
+    Name = "${local.name_prefix}-nat"
+  })
+
+  depends_on = [aws_internet_gateway.gw]
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main_nat_gateway.id
+  }
+
+  tags = merge(local.common_tags, {
+    Name = "${local.name_prefix}-private-rt"
+  })
+}
+
+resource "aws_route_table_association" "private_assoc" {
+  count = length(var.private_subnet_cidrs)
+  subnet_id = aws_subnet.private_subnets.[count.index].id
+  route_table_id = aws_route_table.private_rt.id
 }
