c76290fea49a1f6f202285120d0164fb31158f1a
Alberto <monroyalberto@gmail.com>
2026-05-06 12:02:39 -0600

    Add EKS to the mix

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -3,3 +3,13 @@ module "prod_network" {
   project     = "bto"
   environment = "prod"
 }
+
+module "eks" {
+  source      = "../../modules/EKS"
+  project     = "bto"
+  environment = "prod"
+
+  vpc_id             = module.networking.vpc_id
+  private_subnet_ids = module.networking.private_subnet_ids
+  public_subnet_ids  = module.networking.public_subnet_ids
+}
