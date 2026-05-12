3ab6d2ba5078859cf8317ba27b36f93f24945917
Alberto <monroyalberto@gmail.com>
2026-05-12 15:53:16 -0600

    change made to workflow

--- environments/prod/providers.tf
+++ environments/prod/providers.tf
@@ -18,8 +18,6 @@ terraform {
   }
 }
 
-
-
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
